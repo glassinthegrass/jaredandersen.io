d18d52c10376b0eb7518b76551e9a45ccca64dd6
Jared Andersen <j-mac@Jareds-MBP.attlocal.net>
2022-01-20 11:31:20 -0500

    imported createBrowserHistory function from history. created custom history and passed it to my Router component.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
 import App from "./App";
 import "./reset.css";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { HashRouter, BrowserRouter } from "react-router-dom";
+const customHistory = createBrowserHistory();
 const Router =
   process.env.NODE_ENV === "development" ? HashRouter : BrowserRouter;
 const root = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router history={customHistory} >
       <GlobalStyle />
       <App />
     </Router>
