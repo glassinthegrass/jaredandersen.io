a6d4ffa97d6b0eacf660ab4ab686bb6098f9ef4e
Jared Andersen <j-mac@Jareds-MBP.attlocal.net>
2022-01-20 11:35:59 -0500

    made implicit return, removed use effect function

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -1,15 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { StyledResume,StyledSpace,StyledFrame } from "./experienceStyles";
 import resume from "../../assets/JaredAndersen-Resume.pdf";
-export const Experience = () => {
-  useEffect(() => {
-    const x = document.querySelector(`#experience`);
-    x.scrollIntoView({ behavior: "smooth" });
-  }, []);
-  return (
+export const Experience = () => (
     <StyledResume >
       <StyledSpace id="experience"/>
       <StyledFrame src={resume} />
     </StyledResume>
   );
-};
