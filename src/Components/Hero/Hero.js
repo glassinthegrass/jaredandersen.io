f00b0ad4833f871cf9917affb2561d1fb8cb0db8
Jared Andersen <j-mac@Jareds-MBP.attlocal.net>
2022-01-20 11:46:47 -0500

    remove use effect function, make Component implicit return

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,23 +1,25 @@
-import React,{useEffect} from "react";
+import React from "react";
 import me from "../../assets/me.jpeg";
-import {StyledHero,StyledBox,StyledCircle,StyledPic,StyledSpace,StyledName,StyledTitle} from "./heroStyles";
+import {
+  StyledHero,
+  StyledBox,
+  StyledCircle,
+  StyledPic,
+  StyledSpace,
+  StyledName,
+  StyledTitle,
+} from "./heroStyles";
 
-export const Hero = ({handleNavigation,id}) => {
-  useEffect(() => {
-    const x = document.querySelector(`#${id}`);
-    x.scrollIntoView({ behavior: "smooth" });
-  }, [id]);
-  return (
-    <StyledHero id="hero">
-      <StyledBox>
-        <StyledCircle>
-          <StyledPic src={me} alt="a" />
-        </StyledCircle>
-        <StyledSpace height="2" />
-        <StyledName>Jared Andersen</StyledName>
-        <StyledTitle>Student, Developer. Father & Husband.</StyledTitle>
-        <StyledSpace />
-      </StyledBox>
-    </StyledHero>
-  );
-};
+export const Hero = () => (
+  <StyledHero id="hero">
+    <StyledBox>
+      <StyledCircle>
+        <StyledPic src={me} alt="a" />
+      </StyledCircle>
+      <StyledSpace height="2" />
+      <StyledName>Jared Andersen</StyledName>
+      <StyledTitle>Student, Developer. Father & Husband.</StyledTitle>
+      <StyledSpace />
+    </StyledBox>
+  </StyledHero>
+);
