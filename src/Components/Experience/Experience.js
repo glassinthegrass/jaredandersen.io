cb0654e1ad5a798498d09b82dcfa2f29b91c9008
Jared Andersen <j-mac@Jareds-MBP.attlocal.net>
2022-01-20 11:35:12 -0500

    changed tag to experience from resume for smooth scroll and navigational purposes

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -3,12 +3,12 @@ import { StyledResume,StyledSpace,StyledFrame } from "./experienceStyles";
 import resume from "../../assets/JaredAndersen-Resume.pdf";
 export const Experience = () => {
   useEffect(() => {
-    const x = document.querySelector(`#resume`);
+    const x = document.querySelector(`#experience`);
     x.scrollIntoView({ behavior: "smooth" });
   }, []);
   return (
-    <StyledResume>
-      <StyledSpace id="resume"/>
+    <StyledResume >
+      <StyledSpace id="experience"/>
       <StyledFrame src={resume} />
     </StyledResume>
   );
