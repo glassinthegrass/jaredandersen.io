2489c0a48c6d2299a95e71b8af4016110689ba52
Jared Andersen <j-mac@Jareds-MBP.attlocal.net>
2022-01-20 11:48:27 -0500

    changed id prop to to prop

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -8,9 +8,9 @@ import { Button } from "./Button";
 export const Menu = ({ menuToggle, handleClick,handleMenu }) => (
     <MenuBox menuToggle={menuToggle}>
       <MenuAside>
-      <Button id='about' icon={me} title='about me' handleClick={handleClick}/>
-      <Button id='portfolio' icon={portfolio} title='portfolio' handleClick={handleClick}/>
-      <Button id='experience' icon={experience} title='experience' handleClick={handleClick}/>
+      <Button to='about' icon={me} title='about me' handleClick={handleClick(menuToggle)}/>
+      <Button to='portfolio' icon={portfolio} title='portfolio' handleClick={handleClick(menuToggle)}/>
+      <Button to='experience' icon={experience} title='experience' handleClick={handleClick(menuToggle)}/>
       </MenuAside>
       <FadedAside onClick={handleMenu}></FadedAside>
     </MenuBox>
