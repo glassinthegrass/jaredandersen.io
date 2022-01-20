df956f2bcc12d31c886f7c0929e152d1a5ff1bfd
Jared Andersen <j-mac@Jareds-MBP.attlocal.net>
2022-01-20 11:38:23 -0500

    restructure of page and removal of fontsize prop on NavLink. and how id prop was typed.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,36 +1,35 @@
 import React from "react";
-import { StyledHeader,StyledNavBox } from "./headerStyles";
+import { StyledHeader, StyledTitleBox, StyledNavBox } from "./headerStyles";
 import { MenuIcon } from "./MenuIcon";
 import { NavLink } from "./NavLink";
-export const Header = ({handleMenu, menuToggle, handleNavigation, highlight}) => (
+import { NavTitleLink } from "./NavTitleLink";
+export const Header = ({
+  handleMenu,
+  menuToggle,
+  handleNavigation,
+  highlight,
+}) => (
   <StyledHeader menuToggle={menuToggle} highlight={highlight}>
     <MenuIcon toggle={handleMenu} />
-    <NavLink
-      fontSize="21"
-      handleClick={handleNavigation}
-      id={"hero"}
-      title="Jared Andersen"
-    />
-    <StyledNavBox>
-      <NavLink
-        fontSize="16"
+    <StyledTitleBox>
+      <NavTitleLink
         handleClick={handleNavigation}
-        id={"about"}
-        title="About Me"
+        id="hero"
+        title="Jared Andersen"
       />
+    </StyledTitleBox>
+    <StyledNavBox>
+      <NavLink handleClick={handleNavigation} id="about" title="About Me" />
       <NavLink
-        fontSize="16"
         handleClick={handleNavigation}
-        id={"portfolio"}
+        id="portfolio"
         title="Portfolio"
       />
       <NavLink
-        fontSize="16"
         handleClick={handleNavigation}
-        id={"experience"}
+        id="experience"
         title="Experience"
       />
     </StyledNavBox>
   </StyledHeader>
 );
-
