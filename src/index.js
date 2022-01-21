85c2bfc4dbb9f0bb7f253c4a5ca106696317ab76
Jared Andersen <j-mac@Jareds-MBP.attlocal.net>
2022-01-21 15:45:54 -0500

    delete unneded files, minor adjustments to create-react-app files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
 import App from "./App";
+import { HashRouter, BrowserRouter } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import "./reset.css";
 import { GlobalStyle } from "./styles/GlobalStyles";
-import { HashRouter, BrowserRouter } from "react-router-dom";
-const customHistory = createBrowserHistory();
-const Router =
-  process.env.NODE_ENV === "development" ? HashRouter : BrowserRouter;
+
 const root = document.getElementById("root");
+const Router = process.env.NODE_ENV === "development" ? HashRouter : BrowserRouter;
+const customHistory = createBrowserHistory();
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router history={customHistory} >
+    <Router history={customHistory}>
       <GlobalStyle />
       <App />
     </Router>
