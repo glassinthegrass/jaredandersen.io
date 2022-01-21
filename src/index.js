666870735482271b868c5971c4df82093631a372
Jared Andersen <j-mac@Jareds-MBP.attlocal.net>
2022-01-21 15:46:19 -0500

    move reset css.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+//import
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { HashRouter, BrowserRouter } from "react-router-dom";
 import { createBrowserHistory } from "history";
-import "./reset.css";
+import './styles/reset.css'
 import { GlobalStyle } from "./styles/GlobalStyles";
-
+//declarations
 const root = document.getElementById("root");
 const Router = process.env.NODE_ENV === "development" ? HashRouter : BrowserRouter;
 const customHistory = createBrowserHistory();
